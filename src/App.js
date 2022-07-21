8db036025cf4721c3b72378961cf4e862adce7cc
dawitAmicho <dave.amicho@gmail.com>
2022-07-21 10:59:45 -0300

    initial phase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//const Api_Url ='http://www.omdbapi.com?apikey=ac280c84';
+const API='https://online-movie-database.p.rapidapi.com/auto-complete?'
+const App = () => {
+	const searchMovies = async(title)=>{
+		const options = {
+			method: 'GET',
+			headers: {
+				'X-RapidAPI-Key': '7ebbe9f787mshfd41c35f8b85186p120b05jsn7b008d1d0e78',
+				'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
+			}
+		};
+		
+		//fetch('https://online-movie-database.p.rapidapi.com/auto-complete?q=all ', options)
+			//.then(response => response.json())
+			//.then(response => console.log(response))
+			//.catch(err => console.error(err));
+		const response = await fetch(`${API}&q=${title}`,options);
+        const data = await response.json();
+	     console.log(data);
+		}
+
+	useEffect(()=> {
+		searchMovies('dave');
+
+	},[]);
+		return (
+		<h2>helloo there </h2>
+
+	);
 }
 
 export default App;
