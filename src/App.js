5e2489e79a77a56e1cb469d0754eac525ade06ea
dawitAmicho <dave.amicho@gmail.com>
2022-07-28 06:20:35 -0300

    api integration

--- src/App.js
+++ src/App.js
@@ -1,74 +1,67 @@
-import React from 'react';
-import { useEffect } from 'react';
-import './app.css';
-import SearchIcon from './search.svg';
+import React, { useState, useEffect } from "react";
+import MovieCard from "./MovieCard";
+import "./app.css";
+import SearchIcon from "./search.svg";
 
 //const Api_Url ='http://www.omdbapi.com?apikey=ac280c84';
-const API='https://online-movie-database.p.rapidapi.com/auto-complete?'
-const movie1={
+const API = "https://online-movie-database.p.rapidapi.com/auto-complete?";
+/*const movie= {
 	"Title": "Amazing Spiderman Syndrome",
-	"Year" : "2012",
+	"Year" : "2012", 
 	"imdbID":"ss",
 	"Type": "movie",
-	"Poster":"N/A",
-}
+	"Poster":"N/A"
+}*/
+
+//fetch('https://online-movie-database.p.rapidapi.com/auto-complete?q=all ', options)
+//.then(response => response.json())
+//.then(response => console.log(response))
+//.catch(err => console.error(err));
+
 const App = () => {
-	const searchMovies = async(title)=>{
-		const options = {
-			method: 'GET',
-			headers: {
-				'X-RapidAPI-Key': '7ebbe9f787mshfd41c35f8b85186p120b05jsn7b008d1d0e78',
-				'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
-			}
-		};
-		
-		//fetch('https://online-movie-database.p.rapidapi.com/auto-complete?q=all ', options)
-			//.then(response => response.json())
-			//.then(response => console.log(response))
-			//.catch(err => console.error(err));
-		const response = await fetch(`${API}&q=${title}`,options);
-        const data = await response.json();
-	     console.log(data);
-		}
+  const [movies, setMovies] = useState([]);
 
-	useEffect(()=> {
-		searchMovies('spiderman');
+  const searchMovies = async (title) => {
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "7ebbe9f787mshfd41c35f8b85186p120b05jsn7b008d1d0e78",
+        "X-RapidAPI-Host": "online-movie-database.p.rapidapi.com",
+      },
+    };
+    const response = await fetch(`${API}&q=${title}`, options);
+    const data = await response.json();
+    setMovies(data.d);
+  };
 
-	},[]);
-		return (
-		<div className="app">
-			<hl>Movie Madness</hl>
-			<div className='search'>
-				<input placeholder="search movie"
-				value="spiderman"
-				onChange={()=>{}}
-				/>
-				<img
-				src={SearchIcon}
-				alt="search"
-				/>
+  useEffect(() => {
+    const fetchMovies = async () => {
+      await searchMovies("man");
+    };
+    fetchMovies();
+  }, []);
 
-			</div>
+  return (
+    <div className="app">
+      <h1>Movie Madness</h1>
+      <div className="search">
+        <input placeholder="search movie" value="search" onChange={() => {}} />
+        <img src={SearchIcon} alt="search" onClick={() => {}} />
+      </div>
 
-			<div className="container">
-				<div className='movie'>
-					<div>
-						<p>{movie1.Year}</p>
-					</div>
-					<div>
-						<img src= {movie1.Poster!== 'N/A' ?
-						 movie1.poster :
-						  'https://via.placeholder.com/400' } alt={movie1.title}/>
-
-					</div>
-				</div>
-
-			</div>
-
-		</div>
-
-
-	);
-}
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => {
+            return <MovieCard key={movie.id} movie={movie} />;
+          })}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2> No movies found</h2>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default App;
